8df110733cde535cb31a019b9a99495d2fc4dd41
Max <78194977+maxwellromack@users.noreply.github.com>
2023-11-01 18:34:45 -0400

    added npm and ejs

---
+++ server.js
@@ -0,0 +1,16 @@
+// Load Node modules
+var express = require('express');
+const ejs = require('ejs');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.listen(8080);
+
+// Display Pages
+app.get('/producer', function (req, res) {
+    var username = "user";
+    res.render('pages/producer', {
+        username: username
+    });
+});
